a9147846586ef1d7768f603a18920c332db90c82
Andrew Brooks <andrewgraemebrooks@gmail.com>
2024-11-12 17:32:37 +0000

    Added a header button to add a rss feed

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Tabs } from 'expo-router';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
@@ -13,6 +14,9 @@ export default function TabLayout() {
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+          headerRight: () => (
+            <Ionicons name="add" color={'red'} size={30} style={{ paddingRight: 10 }} />
           ),
         }}
       />
--- mobile-app/app/(tabs)/index.tsx
+++ mobile-app/app/(tabs)/index.tsx
@@ -1,6 +1,29 @@
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from 'expo-router';
+import { useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Ionicons
+          onPress={onPress}
+          name="add"
+          color={'red'}
+          size={30}
+          style={{ paddingRight: 10 }}
+        />
+      ),
+    });
+  }, [navigation]);
+
+  const onPress = () => {
+    console.log('wesh');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
