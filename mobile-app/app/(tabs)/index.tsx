555b851add7bbee25b42812d03f460bfd86abbb0
Andrew Brooks <andrewgraemebrooks@gmail.com>
2025-01-30 17:57:50 +0000

    Stores a rss feed url in state

--- mobile-app/app/(tabs)/index.tsx
+++ mobile-app/app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from 'expo-router';
-import { useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Alert } from 'react-native';
 
 export default function Index() {
   const navigation = useNavigation();
+  const [feeds, setFeeds] = useState<string[]>([]);
   useEffect(() => {
+    const onPress = () => {
+      // e.g. https://lifehacker.com/feed/rss
+      Alert.prompt(
+        'Add a RSS feed',
+        undefined,
+        (newFeed: string) => setFeeds([...feeds, newFeed]),
+        'plain-text',
+      );
+    };
     navigation.setOptions({
       headerRight: () => (
         <Ionicons
@@ -18,15 +27,16 @@ export default function Index() {
         />
       ),
     });
-  }, [navigation]);
-
-  const onPress = () => {
-    console.log('wesh');
-  };
+  }, [feeds, navigation]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      {feeds.length === 0 && <Text style={styles.text}>Home screen</Text>}
+      {feeds.map((feed, index) => (
+        <Text key={index} style={styles.text}>
+          {feed}
+        </Text>
+      ))}
     </View>
   );
 }
@@ -34,12 +44,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: '#fff',
+    color: '#000',
   },
   button: {
     fontSize: 20,
